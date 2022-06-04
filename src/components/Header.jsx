c3b3af8430f390eb34a1a450278bd8b608505000
sajandhakal12 <sajan12dhakal@gmail.com>
2022-06-04 10:31:28 +0545

    change name of file and download

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,29 +4,51 @@ export const Header = ({ value }) => {
   const [timeToRead, setTimeToRead] = useState(0);
   const [noOfWords, setNumberOfWords] = useState(0);
   const [noOfCharacters, setNumberOfCharacters] = useState(0);
+  const [documentName, setDocumentName] = useState("Document.md");
+
+  const downloadTxtFile = () => {
+    const element = document.createElement("a");
+    const file = new Blob([value], {
+      type: "text/markdown",
+    });
+    element.href = URL.createObjectURL(file);
+    element.download = documentName;
+    document.body.appendChild(element);
+    element.click();
+  };
 
   useEffect(() => {
     const text = document.getElementById("preview").innerText;
     const wpm = 225;
-
     const words = text
       .trim()
       .split(/\s+/)
       .filter((word) => String(word)).length;
-
     const time = Math.ceil(words / wpm);
     setTimeToRead(time);
     setNumberOfWords(words);
     setNumberOfCharacters(text.length);
   }, [value]);
+
   return (
     <div className="header uppercase">
       <div className="header-item">
-        <p className="text-muted header-item-element ">DOCUMENT NAME</p>
-        <p className="header-item-element">Document.md</p>
+        <p className="text-muted header-item-element ">
+          DOCUMENT NAME
+          <span onClick={downloadTxtFile} className="download-button">
+            &#8595;
+          </span>
+        </p>
+        <p className="header-item-element">
+          <input
+            className="doc-name-input"
+            value={documentName}
+            onChange={(e) => setDocumentName(e.target.value)}
+          />
+        </p>
       </div>
       <div className="header-item">
-        <p className="header-item-element">
+        <p className="header-item-element text-right">
           <span className="text-muted ">Reading Time:&nbsp;</span>
           <span className="">{timeToRead} MIN READ&nbsp;</span>
           <span className="text-muted ">Words:&nbsp;</span>
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://sajandhakal12.github.io/markdown-editor/",
   plugins: [react()],
 });
