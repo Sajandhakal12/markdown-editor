4fb24d66a1d33a5ffbfdfc917e955506268f9f97
sajandhakal12 <sajan12dhakal@gmail.com>
2022-06-30 09:30:59 +0545

    local store

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Header } from "./components/Header";
 import MarkdownPreview from "./components/MarkdownPreview";
 import TextInput from "./components/TextInput";
@@ -6,6 +6,17 @@ import TextInput from "./components/TextInput";
 
 function App() {
   const [value, setValue] = useState("");
+
+  useEffect(() => {
+    try {
+      const value = localStorage.getItem("value");
+      if (value) setValue(value);
+    } catch (e) {}
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("value", value);
+  }, [value]);
 
   return (
     <div>
